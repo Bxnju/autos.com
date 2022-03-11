a4fd15dc9fb22c162f68599acb6952604cc475c8
Bxnju <andres.benjumea@upb.edu.co>
2022-03-10 23:33:14 -0500

    navbar responsive

--- js/script.js
+++ js/script.js
@@ -48,3 +48,12 @@ function displayUser(user){
 
 }
 
+function myFunction() {
+  var x = document.getElementById("nav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
+
